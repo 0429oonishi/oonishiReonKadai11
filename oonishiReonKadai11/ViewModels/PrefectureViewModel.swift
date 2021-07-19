71dfefe3b38422ddd68397379f47e587243a969b
0429oonishi <oonishireon110@gmail.com>
2021-07-19 12:27:22 +0900

    PrefectureVM実装

--- oonishiReonKadai11/ViewModels/PrefectureViewModel.swift
+++ oonishiReonKadai11/ViewModels/PrefectureViewModel.swift
@@ -6,3 +6,56 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+protocol PrefectureViewModelInput {
+    func cellDidTapppd(name: String)
+    func cancelButtonDidTapped()
+}
+
+protocol PrefectureViewModelOutput: AnyObject {
+    var prefectureNames: [String] { get }
+    var event: Driver<PrefectureViewModel.Event> { get }
+}
+
+protocol PrefectureViewModelType {
+    var inputs: PrefectureViewModelInput { get }
+    var outputs: PrefectureViewModelOutput { get }
+}
+
+final class PrefectureViewModel: PrefectureViewModelInput,
+                                 PrefectureViewModelOutput {
+    
+    enum Event {
+        case cellDidTapped(name: String)
+        case cancelButtonDidTapped
+    }
+    var event: Driver<Event> {
+        eventRelay.asDriver(onErrorDriveWith: .empty())
+    }
+    private let eventRelay = PublishRelay<Event>()
+    
+    let prefectureNames = Prefecture.name
+    
+    func cellDidTapppd(name: String) {
+        eventRelay.accept(.cellDidTapped(name: name))
+    }
+    
+    func cancelButtonDidTapped() {
+        eventRelay.accept(.cancelButtonDidTapped)
+    }
+    
+}
+
+extension PrefectureViewModel: PrefectureViewModelType {
+    
+    var inputs: PrefectureViewModelInput {
+        return self
+    }
+    
+    var outputs: PrefectureViewModelOutput {
+        return self
+    }
+    
+}
